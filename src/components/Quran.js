fbfb034fd706c205e040eca8b72bc60fae902cb9
zozowhmoed <zozowhmo21mohamed@gmail.com>
2025-05-18 18:59:58 +0300

    Update Quran.js

--- src/components/Quran.js
+++ src/components/Quran.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import juzAmmaLast15 from '../data/juzAmmaLast15';
 import '../styles/Quran.css';
 import '../styles/TopButton.css';
